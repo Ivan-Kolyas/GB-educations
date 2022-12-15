09969e3e479fa71b0518982c239ce6eb7727fce0
Ivan-Kolyas <ivandaodao@gmail.com>
2022-12-15 10:39:50 +0300

    Первое задание дз

---
+++ GB Home Works/Home Work1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Проверка двух чисел на наибольшее значение
+
+/*Console.WriteLine("Input number 1");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number 2");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine($"{num1} is > of {num2}");
+}
+
+if (num1 < num2)
+{
+    Console.WriteLine($"{num1} is < of {num2}");
+}
+
+else
+{
+    Console.WriteLine($"{num1} is = of {num2}");
+}*/
+
+
+ 
+ 
+    
