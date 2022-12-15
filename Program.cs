a1c37efae0a21970b4ce24b34d21464629a31777
Ivan-Kolyas <ivandaodao@gmail.com>
2022-12-15 12:02:10 +0300

    второе задание

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿// Проверка двух чисел на наибольшее значение
+
+/*Console.WriteLine("Input number 1");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number 2");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine($"{num1} is > of {num2}");
+}
+
+if (num1 < num2)
+{
+    Console.WriteLine($"{num1} is < of {num2}");
+}
+
+else
+{
+    Console.WriteLine($"{num1} is = of {num2}");
+}*/
+
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+/*Console.WriteLine("Input number 1");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number 2");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number 3");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max;
+ if (num1 >= num2)
+ {
+    if (num3 >= num1)
+    {
+       max = num3;
+    }
+    else
+    {
+       max = num1;
+    }
+ }
+ else
+ {
+    if (num3 >= num2)
+    {
+       max = num3;
+    }
+    else
+    {
+       max = num2;
+    }
+ }
+Console.WriteLine($"{max} is max  of {num1},{num2},{num3}");
+*/
+
+
