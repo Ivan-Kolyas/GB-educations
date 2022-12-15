53905697c506b636d88444eabc98dbe521955d28
Ivan-Kolyas <ivandaodao@gmail.com>
2022-12-15 09:30:45 +0300

    1 семинар

---
+++ GB Seminars/Seminar 1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Запрос целого числа и возведение в квадрат 
+/*Console.WriteLine("Input number");
+int num = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine(a);
+int quad = num * num;
+Console.WriteLine($"Quad of {num} -> {quad}");*/
+
+//Написать программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго
+
+/*Console.WriteLine("Input number 1");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number 2");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int quad = num2 * num2;
+
+if (quad == num1)
+{
+    Console.WriteLine($"{num1} is quad of {num2}");
+}
+else
